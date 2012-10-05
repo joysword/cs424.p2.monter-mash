28a74bcc97ec24898d8dd9af64a2fa8a7201c7ce
claudio <caletti.claudio@gmail.com>
2012-10-04 21:54:14 -0500

    renamed to make class more understandable

---
+++ cc_YearCountPair.java
@@ -0,0 +1,18 @@
+public class YearCountPair{
+  int count;
+  int year_value;
+  
+  public YearCountPair(int count,int year_value){
+    this.count=count;
+    this.year_value=year_value;
+  }
+  
+  public int getCount(){
+    return count; 
+  }
+  
+  public int getYear(){
+    return year_value;
+  }
+  
+}
