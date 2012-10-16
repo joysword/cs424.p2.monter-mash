77d9a97da5bc746140a4e22bb70dd4c22515c4f6
Shi Yin <syin8@uic.edu>
2012-10-15 19:44:12 -0500

    instance.java

--- Instance.java
+++ Instance.java
@@ -1,17 +1,17 @@
 public class Instance{
-	private int[] count={0,0,0,0,0,0,0];
+	private int[] count={0,0,0,0,0,0,0};
 	private int year;
 
 	public Instance(int year){
 		this.year=year;
 	}
 
-	public get(int i){
-		return count(i);
+	public int get(int i){
+		return count[i];
 	}
 
-	public set(int i,int value){
-		count(i)=value;
+	public void set(int i,int value){
+		count[i]=value;
 	}
 
 
